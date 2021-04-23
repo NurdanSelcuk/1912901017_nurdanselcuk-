17751aa9d3f4491309392fa3f7a105b4fd45d5d4
nurdan selçuk <nurdan--selcuk@hotmail.com>
2021-04-23 13:27:42 +0300

    label ve buton ekledim

--- 1912901017_nurdanselcuk/Form1.cs
+++ 1912901017_nurdanselcuk/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace _1912901017_nurdanselcuk
 {
@@ -15,6 +16,11 @@ namespace _1912901017_nurdanselcuk
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
