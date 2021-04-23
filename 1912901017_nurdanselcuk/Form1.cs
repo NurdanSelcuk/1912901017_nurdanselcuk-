caec06d6b18b4d2bfbea23507042b2daf011fe8d
nurdan selçuk <nurdan--selcuk@hotmail.com>
2021-04-23 14:32:56 +0300

    butona kod ekleyım baslık yazdırdım

--- 1912901017_nurdanselcuk/Form1.cs
+++ 1912901017_nurdanselcuk/Form1.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml;
+using System.IO;
+using System.Net;
 
 namespace _1912901017_nurdanselcuk
 {
@@ -19,6 +21,33 @@ namespace _1912901017_nurdanselcuk
         }
 
         private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string xmladresi= "https://www.sabah.com.tr/rss/ekonomi.xml";
+            WebRequest iste = WebRequest.Create(xmladresi);
+           // WebRequest istek = HttpWebRequest.Create(xmladresi);
+            WebResponse yanıt;
+            yanıt = iste.GetResponse();
+            StreamReader bilgiler = new StreamReader(yanıt.GetResponseStream());
+            string gelen = bilgiler.ReadToEnd();
+            int baslikbaslangic = gelen.IndexOf("<title>") + 7;
+            int baslikbitis = gelen.Substring(baslikbaslangic).IndexOf("</title>");
+            string baslik = gelen.Substring(baslikbaslangic, baslikbitis);
+            MessageBox.Show(baslik);
+
+
+            
+            //var XMLdokuman = new XmlDocument();
+           // XMLdokuman.Load(baslik);
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
         {
 
         }
